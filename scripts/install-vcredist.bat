e39f4c40fec2975888ec4344f5879d41198a8673
Maximilian Gindorfer <maximilian.gindorfer@delta4x4.com>
2026-02-12 16:20:00 +0100

    refactor(scripts): setup `install-vcredist`

---
+++ scripts/install-vcredist.bat
@@ -0,0 +1,6 @@
+@echo off
+
+@REM This script installs the Visual C++ Redistributable (vcredist) on the system.
+@REM It checks if vcredist is already installed and if not, it downloads and installs 
+@REM it using the latest release obtained from the official Microsoft website.
+
